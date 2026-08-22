be3a7160526d8a92de31892ead9f105be5c72197
tyduc45 <binaryhaze098@gmail.com>
2026-08-22 21:19:38 +1000

    添加项目文件。

---
+++ Source/pdd_cloth/pdd_cloth.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "pdd_cloth.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, pdd_cloth, "pdd_cloth" );
